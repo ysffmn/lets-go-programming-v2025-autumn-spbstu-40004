3d7dedff897e6b0a82994fb3ff9345f986e1201f
ysffmn <andrewnikonov387@gmail.com>
2026-01-08 03:17:59 +0300

    [TASK-8] quick fix

--- andrew.nikonov/task-8/config/config_dev.go
+++ andrew.nikonov/task-8/config/config_dev.go
@@ -1,7 +1,11 @@
+//go:build dev
+// +build dev
+
 package config
 
 import _ "embed"
 
+//go:embed dev.yaml
 var devYAML []byte
 
 func rawYAML() []byte { return devYAML }
--- andrew.nikonov/task-8/config/config_prod.go
+++ andrew.nikonov/task-8/config/config_prod.go
@@ -1,7 +1,11 @@
+//go:build !dev
+// +build !dev
+
 package config
 
 import _ "embed"
 
+//go:embed prod.yaml
 var prodYAML []byte
 
 func rawYAML() []byte { return prodYAML }
